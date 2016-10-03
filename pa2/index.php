ba97874a534ce6bc36dd7452fef4e6eac73caa1c
mibzman <27thisotope@gmail.com>
2016-10-03 15:36:32 -0400

    finished email checker

--- pa2/index.php
+++ pa2/index.php
@@ -5,6 +5,13 @@
 		<p> factorial output: </p>
 		<font id = "output"></font>
 
+		<form>
+			Enter email:
+			<input id = "email1" type="text">
+			<input type="button" value="Validate" onClick="Validate();">
+			<font id = "email1Output"></font>
+		</form>
+
 <SCRIPT LANGUAGE = "Javascript">
 	function TakeInput(){
 		var value = parseInt(prompt("Please enter a positive number", "8"));
@@ -30,6 +37,103 @@
 		return input * output;
 	}
 
+
+	function Validate(){
+		var email = document.getElementById("email1").value;
+		var local = email.match(/.+(?=@)/);
+		var newEmail = "";
+		
+		for (i = 0; i < local[0].length; i++) {
+			//newEmail += local[0][i].strike();
+			var output = !IsValidChar(local[0][i]);
+			if (output){
+				newEmail += local[0][i].strike();
+			}else{
+				newEmail += local[0][i];
+			}
+			
+		}
+		newEmail = CheckDoublePeriods(newEmail);
+		
+		var afterAt = email.substring(email.indexOf("@") + 1);
+		var newAfter = "";
+		var j = 0;
+		if (afterAt[0] == "-"){
+			newAfter += afterAt[0].strike(); //reeeealy hard to see, but not allowed
+			j = 1;
+                }
+		
+		
+		for (j; j < afterAt.length; j++){
+			var output = !IsValidDomainChar(afterAt[j]);
+                        if (output){
+                                newAfter += afterAt[j].strike();
+                        }else{
+                                newAfter += afterAt[j];
+                        }
+		}
+			
+
+		document.getElementById("email1Output").innerHTML = newEmail + "@" + newAfter;
+
+	}
+	
+	function CheckDoublePeriods(newEmail){
+		var doublePeriod = newEmail.indexOf("..");
+                if (doublePeriod > 0){
+                        var str1 = newEmail.substring(0, doublePeriod);
+                        var offender = newEmail.substring(doublePeriod, doublePeriod + 2);
+                        var str2 = newEmail.substring(doublePeriod + 2);
+                        //newEmail = str1 + offender.strike() + str2;
+                        return str1 + offender.strike() +  CheckDoublePeriods(str2);
+                }else {
+			return newEmail;
+		}
+
+	}
+
+	function IsValidDomainChar(n){
+
+		if (isNumeric(n)){
+               	        return true;
+                }
+                if (isLetter(n)){
+                        return true;
+                }
+                if (n.length === 1 && n.match(/[-]|[.]/)){
+                        return true;
+                }
+
+                return false;
+	}
+	
+	
+	function IsValidChar(n){
+	
+		if (isNumeric(n)){
+			return true;
+		}
+		if (isLetter(n)){
+			return true;
+		}
+		if (isSpecial(n)){
+			return true;
+		}
+	
+		return false;
+	}
+	
+	function isNumeric(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+	
+	function isLetter(n){
+		return n.length === 1 && n.match(/[a-z]/i);
+	}
+	
+	function isSpecial(n) {
+		return n.length === 1 && n.match(/[{]|[}]|[!]|[#]|[$]|[%]|[&]|[']|[*]|[+]|[-]|[\/]|[=]|[?]|[\^]|[_]|[`]|[|]|[~]|[.]/);
+	}
 
 
 	var val = TakeInput();
